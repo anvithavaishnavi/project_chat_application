6987b1e600371e35271981cb4f7070cb831736ce
Vaishnavi <anvithavaishnavi9169@gmail.com>
2025-11-27 20:55:21 +0530

    Update backend endpoint to Render

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,7 +9,8 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+const ENDPOINT = 'https://chat-backend-gg2g.onrender.com';
+
 
 let socket;
 
